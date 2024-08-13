e3160ae5a3763c4dd269ad23e34056e0291626cd
LeonardoGNascimento <leonardogabrielllpedroso@gmail.com>
2024-08-12 23:00:25 -0300

    botao compra

--- resources/views/filament/pages/plano.blade.php
+++ resources/views/filament/pages/plano.blade.php
@@ -39,7 +39,7 @@
                     <p>Your balance is increased by</p>
                     <h2 class="text-2xl font-bold text-primary-500">R$ 5000</h2>
                     <div class="mt-3">
-                        <a href="https://playconnectapi.com/agent/checkout/6"
+                        <a href="#"
                             class="fi-btn relative grid-flow-col items-center justify-center font-semibold outline-none transition duration-75 focus-visible:ring-2 rounded-lg fi-color-gray fi-btn-color-gray fi-size-md fi-btn-size-md gap-1.5 px-3 py-2 text-sm sm:inline-grid shadow-sm bg-white text-gray-950 hover:bg-gray-50 dark:bg-white/5 dark:text-white dark:hover:bg-white/10 ring-1 ring-gray-950/10 dark:ring-white/20">
                             Comprar agora
                         </a>
